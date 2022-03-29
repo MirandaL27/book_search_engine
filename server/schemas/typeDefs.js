cc4232c6c3445c22e08d53ebd5af29ed93fd9c99
Cadenza <mirandalucsb@gmail.com>
2022-03-29 16:48:54 -0700

    added code to auth middleware, resolvers, and typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,47 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type User {
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+}
+
+type Book {
+  bookId: int
+  authors: [String]
+  description: String
+  title: string
+  image: string 
+  link: string
+}
+
+input BookInput {
+  bookId: Int
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
+
   type Query {
-    helloWorld: String
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: ID!): User
   }
 `;
 
